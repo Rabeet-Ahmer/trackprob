1e9f408185d759474d7a17d295a37487edfd0a02
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2025-07-18 21:00:08 +0500

    Set up Radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const geistSans = Geist({
@@ -15,7 +17,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "TrackProb | Issue Tracker App",
-  description: "A modern, fast, and minimal issue tracking app built with Next.js.",
+  description:
+    "A modern, fast, and minimal issue tracking app built with Next.js.",
 };
 
 export default function RootLayout({
@@ -28,8 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
